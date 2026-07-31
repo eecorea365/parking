6de4ba44cd905c367cfcf6024637c02b75c495a1
Bumsuk Kim <eecorea@naver.com>
2026-07-31 11:18:56 +0900

    [v6.0] 설정 파일 분리 및 config 구조화

--- js/config.js
+++ js/config.js
@@ -1,11 +1,74 @@
 const config = {
 
-    owner: "김범석",
+    // 차량 소유자 정보
+    owner: {
+        name: "김범석",
+        phone: "01072607368"
+    },
 
-    phone: "010-7260-7368",
 
-    vehicle: "Tesla Model Y",
+    // 차량 정보
+    vehicle: {
+        model: "Tesla Model Y",
+        status: "charging"
+    },
 
-    status: "charging"
 
-}
+    // 기본 문자 메시지
+    message: {
+        sms:
+`안녕하세요.
+차량 이동 부탁드립니다.`
+    },
+
+
+    // 차량 상태 목록
+    statusMap: {
+
+        available: {
+
+            emoji: "🟢",
+            title: "현재 차량 확인 가능",
+            desc: "평균 응답시간 : 약 5분"
+
+        },
+
+
+        charging: {
+
+            emoji: "🔋",
+            title: "충전 중",
+            desc: "충전이 끝나는 즉시 이동하겠습니다."
+
+        },
+
+
+        shopping: {
+
+            emoji: "🛒",
+            title: "쇼핑 중",
+            desc: "확인 후 빠르게 이동하겠습니다."
+
+        },
+
+
+        meal: {
+
+            emoji: "🍽️",
+            title: "식사 중",
+            desc: "잠시만 기다려 주세요."
+
+        },
+
+
+        driving: {
+
+            emoji: "🚗",
+            title: "운전 중",
+            desc: "안전한 곳에서 확인하겠습니다."
+
+        }
+
+    }
+
+};
