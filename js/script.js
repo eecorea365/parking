0a0b0b019ec21abcbf880294ff6b1e0cabb61ef0
Bumsuk Kim <eecorea@naver.com>
2026-07-31 19:22:53 +0900

    [v8.0] PWA 적용

--- js/script.js
+++ js/script.js
@@ -54,3 +54,33 @@ function changeStatus(newStatus) {
     updateStatus();
 
 }
+
+if ("serviceWorker" in navigator) {
+
+    window.addEventListener(
+        "load",
+        () => {
+
+            navigator.serviceWorker
+            .register("./service-worker.js")
+            .then(
+                registration => {
+                    console.log(
+                      "Service Worker 등록 완료",
+                      registration
+                    );
+                }
+            )
+            .catch(
+                error => {
+                    console.log(
+                      "Service Worker 실패",
+                      error
+                    );
+                }
+            );
+
+        }
+    );
+
+}
